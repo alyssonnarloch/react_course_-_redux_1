9884af3b6510df5d2d2ce8b2609ac19081855e52
Alysson Narloch <alysson.narloch@gmail.com>
2018-02-26 23:25:19 -0300

    Projeto completo.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { 'title': 'Javascript', pages: 200 },
+        { 'title': 'C#', pages: 59 },
+        { 'title': 'PHP', pages: 302 },
+        { 'title': 'MySQL', pages: 502 }
+    ];
+}
